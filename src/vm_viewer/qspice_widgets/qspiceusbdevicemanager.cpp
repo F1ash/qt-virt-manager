72b0fe342ea8750683a6b6b43faa7abb4da554ea
F1ash <kaperang07@gmail.com>
2015-10-05 00:55:23 +0300

    saved current state;

--- src/vm_viewer/qspice_widgets/qspiceusbdevicemanager.cpp
+++ src/vm_viewer/qspice_widgets/qspiceusbdevicemanager.cpp
@@ -17,14 +17,20 @@ void QSpiceHelper::auto_connect_failed(SpiceUsbDeviceManager *manager,
                                        GError                *error,
                                        gpointer               user_data)
 {
-
+    Q_UNUSED(manager)
+    QSpiceUsbDeviceManager *_manager =
+            static_cast<QSpiceUsbDeviceManager *>(user_data);
+    emit _manager->autoConnectFailed();
 }
 
 void QSpiceHelper::device_added(SpiceUsbDeviceManager *manager,
                                 SpiceUsbDevice        *device,
                                 gpointer               user_data)
 {
-
+    Q_UNUSED(manager)
+    QSpiceUsbDeviceManager *_manager =
+            static_cast<QSpiceUsbDeviceManager *>(user_data);
+    emit _manager->deviceAdded();
 }
 
 void QSpiceHelper::device_error(SpiceUsbDeviceManager *manager,
@@ -32,31 +38,88 @@ void QSpiceHelper::device_error(SpiceUsbDeviceManager *manager,
                                 GError                *error,
                                 gpointer               user_data)
 {
-
+    Q_UNUSED(manager)
+    QSpiceUsbDeviceManager *_manager =
+            static_cast<QSpiceUsbDeviceManager *>(user_data);
+    emit _manager->deviceError();
 }
 
 void QSpiceHelper::device_removed(SpiceUsbDeviceManager *manager,
                                   SpiceUsbDevice        *device,
                                   gpointer               user_data)
 {
-
+    Q_UNUSED(manager)
+    QSpiceUsbDeviceManager *_manager =
+            static_cast<QSpiceUsbDeviceManager *>(user_data);
+    emit _manager->deviceRemoved();
 }
-
 
 void QSpiceUsbDeviceManager::init(QSpiceSession *session)
 {
-    GError **err = NULL;
     SpiceSession *_session = static_cast<SpiceSession *>(session->gobject);
     if ( _session ) {
+        GError **err = NULL;
         gobject = spice_usb_device_manager_get(_session, err);
-        g_signal_connect(gobject, "auto-connect-failed",
-                         (GCallback) QSpiceHelper::auto_connect_failed, this);
-        g_signal_connect(gobject, "device-added",
-                         (GCallback) QSpiceHelper::device_added, this);
-        g_signal_connect(gobject, "device-error",
-                         (GCallback) QSpiceHelper::device_error, this);
-        g_signal_connect(gobject, "device-removed",
-                         (GCallback) QSpiceHelper::device_removed, this);
-        qDebug()<<"usb device manager initiated";
+        if ( gobject ) {
+            g_signal_connect(gobject, "auto-connect-failed",
+                             (GCallback) QSpiceHelper::auto_connect_failed, this);
+            g_signal_connect(gobject, "device-added",
+                             (GCallback) QSpiceHelper::device_added, this);
+            g_signal_connect(gobject, "device-error",
+                             (GCallback) QSpiceHelper::device_error, this);
+            g_signal_connect(gobject, "device-removed",
+                             (GCallback) QSpiceHelper::device_removed, this);
+            qDebug()<<"UsbDeviceManager initiated";
+        } else {
+            qDebug()<<"UsbDeviceManager not initiated";
+        }
     };
+}
+
+void* QSpiceUsbDeviceManager::spice_usb_device_manager_get_devices()
+{
+
+}
+
+void* QSpiceUsbDeviceManager::spice_usb_device_manager_get_devices_with_filter
+                                                (const char*)
+{
+
+}
+
+bool QSpiceUsbDeviceManager::spice_usb_device_manager_is_device_connected
+                                                (void*)
+{
+    return true;
+}
+
+void QSpiceUsbDeviceManager::spice_usb_device_manager_disconnect_device
+                                                (void*)
+{
+
+}
+
+bool QSpiceUsbDeviceManager::spice_usb_device_manager_can_redirect_device
+                                                (void*, void*)
+{
+    return true;
+}
+
+void QSpiceUsbDeviceManager::spice_usb_device_manager_connect_device_async
+                                                (void*, void*, void*, void*)
+{
+
+}
+
+bool QSpiceUsbDeviceManager::spice_usb_device_manager_connect_device_finish
+                                                (void*, void*)
+{
+    return true;
+}
+
+char* QSpiceUsbDeviceManager::spice_usb_device_get_description
+                                                (void*, const char*)
+{
+    char *_desc;
+    return _desc;
 }
--- src/vm_viewer/qspice_widgets/qspiceusbdevicemanager.h
+++ src/vm_viewer/qspice_widgets/qspiceusbdevicemanager.h
@@ -10,6 +10,20 @@ public:
     explicit QSpiceUsbDeviceManager(
             QObject        *parent  = 0,
             QSpiceSession  *s       = 0);
+    void*       spice_usb_device_manager_get_devices();
+    void*       spice_usb_device_manager_get_devices_with_filter
+                                                    (const char*);
+    bool        spice_usb_device_manager_is_device_connected
+                                                    (void*);
+    void                spice_usb_device_manager_disconnect_device
+                                                    (void*);
+    bool        spice_usb_device_manager_can_redirect_device
+                                                    (void*, void*);
+    void        spice_usb_device_manager_connect_device_async
+                                                    (void*, void*, void*, void*);
+    bool        spice_usb_device_manager_connect_device_finish
+                                                    (void*, void*);
+    char*       spice_usb_device_get_description    (void*, const char*);
 
 signals:
     void        autoConnectFailed();
@@ -18,6 +32,7 @@ signals:
     void        deviceRemoved();
 
 protected:
+    friend class QSpiceHelper;
     void        init(QSpiceSession *);
 
 public slots:
